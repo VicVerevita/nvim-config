4f04746b942faa10dcb1b9b58ff54ace60c6d8fa
VicVerevita <0victor.gabriel01@gmail.com>
2024-06-28 23:17:52 +0000

    updated to Kickstart LazyVim custom config

---
+++ lua/custom/options/init.lua
@@ -0,0 +1,36 @@
+vim.o.title = true
+vim.o.titlestring = '%t'
+vim.cmd [[
+  autocmd BufEnter * let &titlestring = expand("%:p")
+]]
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- Hide cmdbar if there is nothing to display
+vim.opt.cmdheight = 0
+-- Keep cursor in the middle of the screen
+vim.o.scrolloff = 999
+-- Set Font size
+vim.g.font_size = 14
+-- Add rust build command
+vim.api.nvim_command 'command! CargoBuildRelease :terminal cargo build --release'
+
+-- Add rust formating on save
+function _G.format_rust_on_save()
+  local params = {
+    textDocument = vim.lsp.util.make_text_document_params(),
+    options = {
+      tabSize = 4,
+      insertSpaces = true,
+    },
+  }
+  vim.lsp.buf_request(0, 'textDocument/formatting', params, nil)
+end
+vim.cmd [[autocmd BufWritePre *.rs lua format_rust_on_save()]]
+
+function _G.md_to_pdf()
+  vim.fn.system(
+    'md-to-pdf ' .. vim.fn.expand '%:p' .. ' --pdf-options=\'{"format": "A4", "margin": {"top": "10mm", "right": "20mm", "bottom": "20mm", "left": "20mm"}}\''
+  )
+end
+vim.cmd [[autocmd BufWritePost *.2pdf.md lua md_to_pdf()]]
+vim.api.nvim_set_keymap('n', '<leader>c', [[:%s/\s\+\(-H\|-\{2,}\)/ \\\r\1/g<CR>]], { noremap = true, silent = true })
---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,130 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  -- MOVE STUFF
+  { 'matze/vim-move' },
+  -- Color preview for working with hex colors
+  { 'rrethy/vim-hexokinase' },
+  -- Theming / add themes here change from init.lua
+  { 'sainnhe/sonokai' },
+  { 'folke/tokyonight.nvim' },
+  --Status line
+  { 'vim-airline/vim-airline' },
+  { 'vim-airline/vim-airline-themes' },
+  -- Copilot
+  { 'github/copilot.vim' },
+  -- Neo tree
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      require('neo-tree').setup {
+        window = {
+          position = 'left',
+          width = 22,
+          mapping_options = {
+            noremap = true,
+            nowait = true,
+          },
+        },
+      }
+    end,
+  },
+  -- Startup screen
+  {
+    'startup-nvim/startup.nvim',
+    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('startup').setup {
+        header = {
+          type = 'text',
+          oldfiles_directory = true,
+          align = 'center',
+          fold_section = false,
+          title = 'Header',
+          margin = 0,
+          content = {
+            '██╗   ██╗██╗ █████╗ ██╗   ██╗███████╗██████╗ ███████╗██╗   ██╗██╗████████╗ █████╗ ',
+            '██║   ██║██║██╔══██╗██║   ██║██╔════╝██╔══██╗██╔════╝██║   ██║██║╚══██╔══╝██╔══██╗',
+            '╚██╗ ██╔╝██║██║  ╚═╝╚██╗ ██╔╝█████╗  ██████╔╝█████╗  ╚██╗ ██╔╝██║   ██║   ███████║',
+            ' ╚████╔╝ ██║██║  ██╗ ╚████╔╝ ██╔══╝  ██╔══██╗██╔══╝   ╚████╔╝ ██║   ██║   ██╔══██║',
+            '  ╚██╔╝  ██║╚█████╔╝  ╚██╔╝  ███████╗██║  ██║███████╗  ╚██╔╝  ██║   ██║   ██║  ██║',
+            '   ╚═╝   ╚═╝ ╚════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝   ╚═╝   ╚═╝  ╚═╝',
+          },
+          highlight = 'Statement',
+          default_color = '',
+          oldfiles_amount = 0,
+        },
+        -- name which will be displayed and command
+        body = {
+          type = 'mapping',
+          oldfiles_directory = false,
+          align = 'center',
+          fold_section = false,
+          title = 'Basic Commands',
+          margin = 0,
+          content = {
+            { ' Find File', 'Telescope find_files', '<leader>ff' },
+            { ' Find Word', 'Telescope live_grep', '<leader>lg' },
+            { ' Recent Files', 'Telescope oldfiles', '<leader>of' },
+            { ' File Browser', 'Telescope file_browser', '<leader>fb' },
+            { ' Colorschemes', 'Telescope colorscheme', '<leader>cs' },
+            { ' New File', "lua require'startup'.new_file()", '<leader>nf' },
+          },
+          highlight = 'String',
+          default_color = '',
+          oldfiles_amount = 0,
+        },
+        footer = {
+          type = 'text',
+          oldfiles_directory = false,
+          align = 'center',
+          fold_section = false,
+          title = 'Footer',
+          margin = 0,
+          content = { 'tragdate.ninja' },
+          highlight = 'Number',
+          default_color = '',
+          oldfiles_amount = 0,
+        },
+
+        options = {
+          mapping_keys = true,
+          cursor_column = 0.5,
+          empty_lines_between_mappings = true,
+          disable_statuslines = true,
+          paddings = { 8, 4, 2, 10 },
+        },
+        mappings = {
+          execute_command = '<CR>',
+          open_file = 'o',
+          open_file_split = '<c-o>',
+          open_section = '<TAB>',
+          open_help = '?',
+        },
+        colors = {
+          background = '#1f2227',
+          folded_section = '#0069ff',
+        },
+        parts = { 'header', 'body', 'footer' },
+      }
+    end,
+  },
+  -- {
+  --   'Exafunction/codeium.vim',
+  --   config = function ()
+  --     -- Change '<C-g>' here to any keycode you like.
+  --     vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+  --     vim.keymap.set('i', '<c-n>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+  --     vim.keymap.set('i', '<c-p>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+  --     vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+  --   end
+  -- }
+}
